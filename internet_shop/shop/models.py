80072f874e0560b32e23aebd52490bcbd0b46f0b
ALEXANDER D <deadcove@mail.ru>
2024-01-23 22:04:27 +0300

    View Product was added. Template Product was created.

--- internet_shop/shop/models.py
+++ internet_shop/shop/models.py
@@ -1,8 +1,23 @@
 from django.db import models
 
 
+def image_product_path(instance, filename):
+    return f"products/{instance.category.name}/{filename}"
+
+
 class Category(models.Model):
     name = models.CharField(max_length=255)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Категория"
+        verbose_name_plural = "Категории"
 
 
 class Product(models.Model):
@@ -11,4 +26,18 @@ class Product(models.Model):
     price = models.FloatField()
     stock = models.IntegerField()
 
+    available = models.BooleanField(default=False)
+    image = models.ImageField(upload_to=image_product_path, blank=True)
+
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.price} р."
+
+    class Meta:
+        ordering = ["name"]
+        verbose_name = "Продукт"
+        verbose_name_plural = "Продукты"
--- internet_shop/shop/views.py
+++ internet_shop/shop/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Product
+
+
+class ProductList(ListView):
+    model = Product
+    template_name = "products.html"
+    context_object_name = 'products'
